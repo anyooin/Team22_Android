7fb3bdc506c9d8b59eabbf445ccac6dce2132433
akuby21 <akuby21@gmail.com>
2024-11-06 16:50:27 +0900

    refactor: refactor architect structure
    data를 Result로 래핑하던 방식에서 viewmodel단에서 catch 하는식으로 변경

--- app/src/main/java/com/team22/soundary/feature/signup/domain/LoginUseCase.kt
+++ app/src/main/java/com/team22/soundary/feature/signup/domain/LoginUseCase.kt
@@ -2,18 +2,23 @@ package com.team22.soundary.feature.signup.domain
 
 import com.team22.soundary.core.domain.TokenRepository
 import com.team22.soundary.core.domain.model.Token
+import com.team22.soundary.core.domain.model.User
+import com.team22.soundary.feature.main.domain.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
-    private val tokenRepository: TokenRepository
+    private val tokenRepository: TokenRepository,
+    private val userRepository: UserRepository
 ) {
-    suspend fun invoke(kakaoToken: String): Flow<Result<Token>> =
-        tokenRepository.loginWithKakao(kakaoToken).map { result ->
-            result.onSuccess {token ->
-                tokenRepository.saveAccessToken(token.accessToken)
-                tokenRepository.saveRefreshToken(token.refreshToken)
-            }
-        }
+    suspend fun invoke(kakaoToken: String): Flow<User> {
+        val token = tokenRepository.loginWithKakao(kakaoToken).first()
+        tokenRepository.saveRefreshToken(token.accessToken)
+        tokenRepository.saveAccessToken(token.refreshToken)
+
+        return userRepository.getMyInfo()
+    }
 }
