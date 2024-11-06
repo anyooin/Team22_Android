a7f58a222d9a8761b773b175708022a68b5e4306
akuby21 <akuby21@gmail.com>
2024-11-06 16:46:22 +0900

    refactor: implement updating user detail

--- app/src/main/java/com/team22/soundary/feature/signup/presentation/LoginUiState.kt
+++ app/src/main/java/com/team22/soundary/feature/signup/presentation/LoginUiState.kt
@@ -1,8 +1,9 @@
 package com.team22.soundary.feature.signup.presentation
 
-sealed class LoginUiState {
-    data object Initial : LoginUiState()
-    data object Loading : LoginUiState()
-    data object Success : LoginUiState()
-    data class Error(val message: String?) : LoginUiState()
+sealed class LoginUiState<out T> {
+    data object Initial : LoginUiState<Nothing>()
+    data object Loading : LoginUiState<Nothing>()
+    data class Success<out T>(val data: T) : LoginUiState<T>()
+    data object Pass : LoginUiState<Nothing>()
+    data class Error(val message: String?) : LoginUiState<Nothing>()
 }
