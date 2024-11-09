b4bd4e0f0f4a8cbd54e9c53890094d97872fa52e
nJiyeon <chxistinn@gmail.com>
2024-11-09 19:29:46 +0900

    refactor: remove FriendRequest.kt

--- app/src/main/java/com/team22/soundary/feature/search/data/FriendRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.team22.soundary.feature.search.data
-
-data class FriendRequest(
-    val id: String = "",
-    val displayId: String = "",
-    val nickname: String = "",
-    val profileImageUrl: String = ""
-)
-
